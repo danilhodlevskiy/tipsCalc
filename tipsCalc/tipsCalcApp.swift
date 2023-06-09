b7aefc318e20a738721051fd0c5a5a470a7e36e8
Danilka <danilkacrypto111@gmail.com>
2023-06-09 09:25:09 +0300

    Initial Commit

---
+++ tipsCalc/tipsCalcApp.swift
@@ -0,0 +1,17 @@
+//
+//  tipsCalcApp.swift
+//  tipsCalc
+//
+//  Created by danilka on 09.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct tipsCalcApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
